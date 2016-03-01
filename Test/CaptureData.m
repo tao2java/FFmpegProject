fb9140eb63bae8cf129294ad1d6b35998faeb6ad
devkiang <********>
2016-03-01 17:53:06 +0800

    更新ffmpeg+x264支持armv7 arm64 ffmpeg更新为3.0版本

--- Test/CaptureData.m
+++ Test/CaptureData.m
@@ -9,6 +9,9 @@
 #import "CaptureData.h"
 #import "avcodec.h"
 #import "swscale.h"
+#import "imgutils.h"
+#import "avformat.h"
+#import "opt.h"
 
 @interface CaptureData ()
 {
@@ -23,6 +26,9 @@
     long _lastTime;
     int fps;
     NSArray *datas;
+    AVCodecContext *context;
+    AVCodec *codec;
+    NSInteger timestamp;
 }
 
 
@@ -31,12 +37,41 @@
 @synthesize labelState;
 
 
+-(void)dealloc
+{
+    avcodec_close(context);
+    av_free(context);
+}
 
 -(id)init
 {
     if(self= [super init])
     {
-        producerFps= 50;
+        producerFps= 30;
+        timestamp=0;
+        avcodec_register_all();
+        codec=avcodec_find_encoder(AV_CODEC_ID_H264);
+        if (!codec) {
+            fprintf(stderr, "h264 codec not found\n");
+            exit(1);
+        }
+        context= avcodec_alloc_context3(codec);
+        context->bit_rate = 240000;
+        context->width = 352;//width;//352;
+        context->height = 288;//height;//288;
+        context->time_base= (AVRational){1,25};
+        context->gop_size = 10;
+        context->max_b_frames=1;
+        context->pix_fmt = AV_PIX_FMT_YUV420P;
+        context->thread_count = 1;
+        AVDictionary * codec_options=NULL;
+        av_dict_set( &codec_options, "preset", "superfast", 0 );
+        av_dict_set(&codec_options, "tune", "zerolatency", 0);
+        if (avcodec_open2(context, codec,&codec_options) < 0) {
+            fprintf(stderr, "could not open codec\n");
+            exit(1);
+        }
+
         
     }
     return self;
@@ -112,13 +147,13 @@
     [labelState setText:@"Starting Video stream"];
     if(self->avCaptureDevice|| self->avCaptureSession)
     {
-        [labelState setText:@"Already capturing"];
+        [labelState setText:@"摄像头正在运行"];
         return;
     }
     
     if((self->avCaptureDevice = [self getFrontCamera]) == nil)
     {
-        [labelState setText:@"Failed to get valide capture device"];
+        [labelState setText:@"获取摄像头失败"];
         return;
     }
     
@@ -126,14 +161,14 @@
     AVCaptureDeviceInput *videoInput = [AVCaptureDeviceInput deviceInputWithDevice:self->avCaptureDevice error:&error];
     if (!videoInput)
     {
-        [labelState setText:@"Failed to get video input"];
+        [labelState setText:@"获取视频输入失败"];
         self->avCaptureDevice= nil;
         return;
     }
     
     self->avCaptureSession = [[AVCaptureSession alloc] init];
-    self->avCaptureSession.sessionPreset = AVCaptureSessionPresetLow;
-    [self->avCaptureSession addInput:videoInput];
+    self->avCaptureSession.sessionPreset = AVCaptureSessionPresetLow;//适用于3G传输
+    [self->avCaptureSession addInput:videoInput];//添加输入设备
     
     // Currently, the only supported key is kCVPixelBufferPixelFormatTypeKey. Recommended pixel format choices are
     // kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange or kCVPixelFormatType_32BGRA.
@@ -146,12 +181,14 @@
     
 //    [self->avCaptureDevice setActiveVideoMinFrameDuration:CMTimeMake(1, self->producerFps)];
     avCaptureVideoDataOutput.videoSettings = settings;
+//    [avCaptureDevice setActiveVideoMinFrameDuration:CMTimeMake(1, self->producerFps)];
     avCaptureVideoDataOutput.minFrameDuration = CMTimeMake(1, self->producerFps);
-    /*We create a serial queue to handle the processing of our frames*/
-    dispatch_queue_t queue = dispatch_queue_create("org.doubango.idoubs", NULL);
+    /*创建一个并行队列绑定到帧数进程上去*/
+    dispatch_queue_t queue = dispatch_queue_create("com.bigkiang.shangjiang", NULL);
     [avCaptureVideoDataOutput setSampleBufferDelegate:self queue:queue];
     [self->avCaptureSession addOutput:avCaptureVideoDataOutput];
     
+    //视频捕捉预览
     AVCaptureVideoPreviewLayer* previewLayer = [AVCaptureVideoPreviewLayer layerWithSession: self->avCaptureSession];
     previewLayer.frame = localView.bounds;
     previewLayer.videoGravity= AVLayerVideoGravityResizeAspectFill;
@@ -185,95 +222,102 @@
     CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
     
     /*帧数处理*/
+    //锁定顶点缓冲区
     //是为了防止程序在向顶点缓冲区中写入顶点数据或修改缓冲区中顶点数据时，显示卡不等待程序输入或修改完毕便直接将这些尚未完成的数据显示到屏幕中的问题（显示卡硬件刷新的速度很多时候会快于程序刷写顶点缓存的速度，也就是说当程序还未完成顶点数据写入时，显卡硬件可能早已经完成了前一帧画面的输出，又回过头来重复操作了），这样会造成图像输出错误等。
     //see http://zhidao.baidu.com/link?url=8vMlKBs5CzQgaIsqzxWeB3crWqdlYIvyEjNElonB9bK48zTNVE3LonDyFhtM_b3DhbzRfponadhGAW7CyC9DsrjMWHtzLUNQbiatnfVViLy
     if(CVPixelBufferLockBaseAddress(pixelBuffer, 0) == kCVReturnSuccess)
     {
-
-        NSDateComponents *model=[self getNowTimeModel];
-        long currTime=[model second];
-        if(currTime!=_lastTime){
-            _lastTime=currTime;
-            NSLog(@"=====> fps:%d",fps);
-            fps=0;
-        }else {
-            fps++;
-    }
-       AVFrame *avFrame=[self convertToFFMpegFrame:sampleBuffer];
-        int width = CVPixelBufferGetWidth(pixelBuffer);
-        int height = CVPixelBufferGetHeight(pixelBuffer);
-        [self encodeh264:avFrame with:width andHeight:height];
-        
-    //        UInt8 *bufferPtr = (UInt8 *)CVPixelBufferGetBaseAddress(pixelBuffer);
-//        size_t buffeSize = CVPixelBufferGetDataSize(pixelBuffer);
-        
-                int pixelFormat = CVPixelBufferGetPixelFormatType(pixelBuffer);
-                switch (pixelFormat) {
-                    case kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange:
-                        //TMEDIA_PRODUCER(producer)->video.chroma = tmedia_nv12; // iPhone 3GS or 4
-//                        NSLog(@"Capture pixel format=NV12");
-                        break;
-                    case kCVPixelFormatType_422YpCbCr8:
-                        //TMEDIA_PRODUCER(producer)->video.chroma = tmedia_uyvy422; // iPhone 3
-//                        NSLog(@"Capture pixel format=UYUY422");
-                        break;
-                    default:
-                        //TMEDIA_PRODUCER(producer)->video.chroma = tmedia_rgb32;
-//                        NSLog(@"Capture pixel format=RGB32");
-                        break;
-                }
+        [self encodeH264:sampleBuffer];
         CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
     }
 }
 
-
 #pragma mark - Private Method
 
--(void)encodeh264:(AVFrame*)avframe with:(int)with andHeight:(int)height
+/**
+ 
+ 编码为h264
+ 
+ */
+-(void)encodeH264:(CMSampleBufferRef)sampleBuffer
 {
-    AVCodec *pCodecH264; //编码器
-    
-    
-    //查找h264编码器
-    avcodec_register_all();
-    pCodecH264 = avcodec_find_encoder(AV_CODEC_ID_H264);
-    if(!pCodecH264)
+
+    CVImageBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    // 访问数据
+    int width = (int)CVPixelBufferGetWidth(pixelBuffer);
+    int height = (int)CVPixelBufferGetHeight(pixelBuffer);
+    int pixelFormat = CVPixelBufferGetPixelFormatType(pixelBuffer);
+    enum AVPixelFormat pix_fmt;
+    if (pixelFormat == kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange)
     {
-        fprintf(stderr, "h264 codec not found\n");
-        exit(1);  
+        pix_fmt = AV_PIX_FMT_NV12;
     }
-    AVCodecContext *c= avcodec_alloc_context3(pCodecH264);
-    c->bit_rate = 3000000;// put sample parameters
-    c->width =with;//
-    c->height = height;//
-    
-    // 帧数率
-    AVRational rate;
-    rate.num = 1;
-    rate.den = 25;
-    c->time_base= rate;//(AVRational){1,25};
-    c->gop_size = 10; // emit one intra frame every ten frames
-    c->max_b_frames=1;
-    c->thread_count = 1;
-    c->pix_fmt = AV_PIX_FMT_NV12;//PIX_FMT_RGB24;
-    //打开编码器
-    
-    if(avcodec_open2(c,pCodecH264,NULL)<0)
+    else
     {
-       NSLog(@"不能打开编码库");
-        exit(1);
+        pix_fmt = AV_PIX_FMT_BGR32;
     }
     
-    //初始化SwsContext
-    AVFrame *dstFrame=av_frame_alloc();
+    AVFrame *pFrame=av_frame_alloc();
+    pFrame->quality = 0;
+    unsigned char *rawPixelBase = (unsigned char *)CVPixelBufferGetBaseAddress(pixelBuffer);
+    av_image_fill_arrays(pFrame->data, pFrame->linesize, rawPixelBase, pix_fmt, width, height, 1);
+    AVFrame* outFrame = av_frame_alloc();
+    int  out_size, size, outbuf_size;
+    uint8_t *outbuf;
+    outbuf_size = 100000;
+    outbuf = (uint8_t *)malloc(outbuf_size);
+    size = context->width * context->height;
+    AVPacket avpkt;//是存储压缩编码数据相关信息的结构体 data：压缩编码的数据。
+//   int nbytes=av_image_get_buffer_size(AV_PIX_FMT_YUV420P, context->width, context->height, 1);
+    int nbytes = avpicture_get_size(AV_PIX_FMT_YUV420P, context->width, context->height);
+    uint8_t* outbuffer = (uint8_t*)av_malloc(nbytes);
+    fflush(stdout);
+    avpicture_fill((AVPicture*)outFrame, outbuffer, AV_PIX_FMT_YUV420P, context->width, context->height);
+//    av_image_fill_arrays(outFrame->data, outFrame->linesize, outbuffer, AV_PIX_FMT_YUV420P, width, height, 1);
+    struct SwsContext* fooContext = sws_getContext(width, height,
+                                                   pix_fmt,
+                                                   context->width, context->height,
+                                                   AV_PIX_FMT_YUV420P,
+                                                   SWS_POINT, NULL, NULL, NULL);
+//    pFrame->data[0]  += pFrame->linesize[0] * (height - 1);
+//    pFrame->linesize[0] *= -1;
+//    pFrame->data[1]  += pFrame->linesize[1] * (height / 2 - 1);
+//    pFrame->linesize[1] *= -1;
+//    pFrame->data[2]  += pFrame->linesize[2] * (height / 2 - 1);
+//    pFrame->linesize[2] *= -1;
     
-    struct SwsContext * scxt = sws_getContext(c->width,c->height,AV_PIX_FMT_NV12,c->width,c->height,PIX_FMT_YUV420P,SWS_POINT,NULL,NULL,NULL);
-    sws_scale(scxt, (const uint8_t**)avframe->data, avframe->linesize, 0, c->height, dstFrame->data, dstFrame->linesize);
-    av_frame_free(&dstFrame);
-    av_frame_free(&avframe);
-    avcodec_free_context(&c);
-    sws_freeContext(scxt);
+    sws_scale(fooContext,(const uint8_t**)pFrame->data, pFrame->linesize, 0, height, outFrame->data, outFrame->linesize);
+    // Here is where I try to convert to YUV
+//    NSLog(@"xxxxx=====%d",xx);
+    
+    /* encode the image */
+    int got_packet_ptr = 0;
+    av_init_packet(&avpkt);
+    avpkt.size = outbuf_size;
+    avpkt.data = outbuf;
+    outFrame->pts=timestamp;
+    timestamp++;
+    out_size = avcodec_encode_video2(context, &avpkt, outFrame, &got_packet_ptr);
+
+    
+//    printf("encoding frame (size=%5d)\n", out_size);
+    printf("encoding frame %s\n", avpkt.data);
+//    AVFormatContext *outFormatContext=avformat_alloc_context();
+//    avp
+    
+    
+//    NSLog(@"code end");
+    
+//    av_free(pFrame);
+//    av_free(outFrame);
+    av_frame_free(&pFrame);
+    av_frame_free(&outFrame);
+    av_packet_unref(&avpkt);
+    sws_freeContext(fooContext);
+    free(outbuf);
+    free(outbuffer);
 }
+
 
 
 
@@ -307,7 +351,7 @@
     
     AVFrame *pFrame;
     pFrame = av_frame_alloc();
-    avpicture_fill((AVPicture*)pFrame, rawPixelBase, AV_PIX_FMT_NV12, width, height);
+    int errorCode=avpicture_fill((AVPicture*)pFrame, rawPixelBase, AV_PIX_FMT_NV12, width, height);
     return pFrame;
 }
 
